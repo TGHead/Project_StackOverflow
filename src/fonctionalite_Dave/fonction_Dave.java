ec0c8c2be4760366712f62ee56b5259866c942a4
alexistse <alexis.leniau@free.fr>
2016-11-02 15:04:27 +0100

    avec proxy

--- src/fonctionalite_Dave/fonction_Dave.java
+++ src/fonctionalite_Dave/fonction_Dave.java
@@ -8,6 +8,10 @@ import Data_Structure.*;
 
 public class fonction_Dave {
 	public static void main(String[] args) throws IOException, JSONException {
+    	System.setProperty("http.proxyHost", "cache.univ-st-etienne.fr");
+    	System.setProperty("http.proxyPort", "3128");
+    	System.setProperty("http.proxyHost", "cache.univ-st-etienne.fr");
+    	System.setProperty("http.proxyPort", "3128");
 		String s = HttpRequest.sendGet("https://api.stackexchange.com/2.2/tags/java/top-answerers/all_time", "page=1&pagesize=5&site=stackoverflow");
         Map JSON_Map = JSON_Converter.jsonToMap(s);
 		System.out.println(JSON_Map);
