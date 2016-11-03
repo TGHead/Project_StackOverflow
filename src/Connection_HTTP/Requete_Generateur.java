0b1bea89378f4499db61f0dbd25a10b844886c66
Haoming ZHAN <haoming.zhan@telecom-st-etienne.fr>
2016-11-03 15:23:27 +0100

    Un truc temporelle pour afficher des donnees pour la 1er iteration.

--- src/Connection_HTTP/Requete_Generateur.java
+++ src/Connection_HTTP/Requete_Generateur.java
@@ -4,32 +4,29 @@ import java.util.Scanner;
 
 public class Requete_Generateur {
 	
-	public static String Fonction_Tags(int flag){
+	public static String Fonction_Tags(int flag) throws Exception{
+		String URL = "https://api.stackexchange.com/2.2/tags";
+		if (flag == 0){
+			return URL;
+		}
+		else {
+			throw new Exception("Passer une parametre illegale!");
+		}
+		//System.out.println(requete);
+	}
+	
+	public static String Fonction_Tags(int flag, String para){
 		String URL = "https://api.stackexchange.com/2.2/tags";
 		switch (flag){
-			case 0: 
-				return URL;
 			case 1:
-				URL += GetTop_answers();
+				URL += "/" + para + "/top-answerers/all_time";
 		}
 		return URL;
 		//System.out.println(requete);
 	}
 	
-	public static String GetTop_answers(){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Veuillez entrer le sujet.");
-		String sujet = sc.next();
-		String partie1 =  "/" + sujet + "/top-answerers/all_time";
-		//System.out.println(requete);
-		return partie1;
-	}
-	
-	public static String GET_Parameters() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Veuillez entrer le nombre de personnes a chercher.");
-		String nombre = sc.next();
-		String Parameter = "page=1&pagesize=" + nombre + "&site=stackoverflow";
+	public static String GET_Parameters(String para) {
+		String Parameter = "page=1&pagesize=" + para + "&site=stackoverflow";
 		return Parameter;
 	}
 }
--- src/fonctionalite_Dave/fonction_Dave.java
+++ src/fonctionalite_Dave/fonction_Dave.java
@@ -1,23 +1,80 @@
 package fonctionalite_Dave;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import javax.sql.rowset.spi.TransactionalWriter;
+
 import org.json.JSONException;
 
 import Connection_HTTP.*;
 import Data_Structure.*;
+import IO_operation.Input_Keyboard;
 import Interface_Graphique.*;
 
 public class fonction_Dave {
+	
+	public static String Tri_resultat(String choix, Map json_map) throws Exception{
+		String Str = null;
+		ArrayList<Map<String,Object>> sortList = (ArrayList)json_map.get("items");
+		if (choix.equals("1")){
+			for(int i = 0; i < sortList.size(); i++){  
+			    for(int j = i + 1; j < sortList.size(); j++){  
+			    	if((Integer)sortList.get(i).get("post_count") < (Integer)sortList.get(j).get("post_count")){  
+			    		Map<String, Object> temp = sortList.get(i);  
+			    		sortList.set(i, sortList.get(j));  
+			    		sortList.set(j, temp);  
+			    	}  
+			    }  
+			}  
+		}
+		if (choix.equals("2")) {
+			for(int i = 0; i < sortList.size(); i++){  
+			    for(int j = i + 1; j < sortList.size(); j++){  
+			    	if((Integer)sortList.get(i).get("score") < (Integer)sortList.get(j).get("score")){  
+			    		Map<String, Object> temp = sortList.get(i);  
+			    		sortList.set(i, sortList.get(j));  
+			    		sortList.set(j, temp);  
+			    	}  
+			    }  
+			}  
+		}
+		else {
+			//throw new Exception("parametre illegale!");
+		}
+		for(int i = 0; i < sortList.size(); i++){
+			Str += i + ". Name: " + ((Map) sortList.get(i).get("user")).get("display_name") + "\n" 
+				+ " post_count: " + sortList.get(i).get("post_count") + "\n" 
+				+ " score: " + sortList.get(i).get("score") + "\n"
+				+ " link: " + ((Map) sortList.get(i).get("user")).get("link") + "\n";
+		}
+		return Str;
+	}
+	
 	public static void main(String[] args) throws IOException, JSONException {
 		
-		String s = HttpRequest.sendGet(Requete_Generateur.Fonction_Tags(1),Requete_Generateur.GET_Parameters());
+		String sujet = Input_Keyboard.Get_String("Veuillez entrer un sujet.");
 		
-        Map JSON_Map = JSON_Converter.jsonToMap(s);
+		String nombre = Input_Keyboard.Get_String("Veuillez entrer le nombre de personnes a chercher.");
+		
+		String json_str = HttpRequest.sendGet(Requete_Generateur.Fonction_Tags(1,sujet),Requete_Generateur.GET_Parameters(nombre));
+		
+        Map JSON_Map = JSON_Converter.jsonToMap(json_str);
         
-        List<User> ulist = JSON_Converter.User_fromJMap(JSON_Map);
-		System.out.println(ulist);
-		//resultat_Frame f = new resultat_Frame("Fonction_Dave",ulist.toString());
+        String choix = Input_Keyboard.Get_String("Veuillez choisir la methode pour trier:\n1. Par le nombre de poser des reponses\n2. Par des scores qu'il est recu\n");
+        
+        String result = null;
+		try {
+			result = Tri_resultat(choix,JSON_Map);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        //List<User> ulist = JSON_Converter.User_fromJMap(JSON_Map);
+		System.out.println(result);
+		resultat_Frame f = new resultat_Frame("Fonction_Dave",result);
 	}
 }
