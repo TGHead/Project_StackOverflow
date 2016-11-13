bfd23d3eed6f4b2050a01024a8d3457501f4fd0b
ALEXIS <ALEXIS@ALEXIS-HP.home>
2016-11-13 18:10:57 +0100

    Fonctions Dave avec interface graphique

--- src/Interface_Graphique/ChoixTag.java
+++ src/Interface_Graphique/ChoixTag.java
@@ -1,44 +1,66 @@
 package Interface_Graphique;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import org.json.JSONException;
 
+import fonctionalite_Dave.fonction_Dave;
+
 public class ChoixTag {
-	JFrame frame = new JFrame("Choix du sujet");
-	JButton bouton2 = new JButton("Nombre de réponses ");
-	JButton bouton3 = new JButton("Score obtenu");
-	JPanel panel = new JPanel();
-	JPanel panel2 = new JPanel();
-	JPanel bouton = new JPanel();
-	JPanel methode1 = new JPanel();
-	JPanel methode2 = new JPanel();
-	JPanel t = new JPanel();
-	JTextField tags = new JTextField(10);
-	JButton bouton1 = new JButton("Valider");
-	JPanel texte = new JPanel();
-	JLabel text = new JLabel("Choisissez un sujet");
+	JFrame frame = new JFrame("Dave");
+	JPanel panelNbSujet = new JPanel();
+	JPanel panelNomSujet = new JPanel();
+	JPanel panelNbResultat = new JPanel();
+	JPanel panelMethode = new JPanel();
+	JPanel panelResulat = new JPanel();
+	JButton valider = new JButton("Valider");
+	JButton valider2 = new JButton("Valider");
+	JButton valider3 = new JButton("Valider");
+	JButton methode1 = new JButton("Trier par nombre de post");
+	JButton methode2 = new JButton("Trier par score obtenu");
+	JTextField textField = new JTextField(10);
+	JLabel erreur = new JLabel("le nombre de sujet doit être un nombre entier");
+	int nbSujet;
+	ArrayList<String> sujet_list = new ArrayList<String>();
+	String nbResultat;
+	String choix;
 
 	public ChoixTag() throws IOException, JSONException {
-		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-		texte.add(text);
-		t.add(tags);
-		bouton.add(bouton1);
-		panel.add(texte);
-		panel.add(t);
-		panel.add(bouton);
+		panelNbSujet.setLayout(new BoxLayout(panelNbSujet, BoxLayout.PAGE_AXIS));
+		panelNomSujet.setLayout(new BoxLayout(panelNomSujet, BoxLayout.PAGE_AXIS));
+		panelNbResultat.setLayout(new BoxLayout(panelNbResultat, BoxLayout.PAGE_AXIS));
+		panelMethode.setLayout(new BoxLayout(panelMethode, BoxLayout.PAGE_AXIS));
+		JLabel label = new JLabel("Combien de sujet voulez-vous rechercher ?");
+		JPanel panelLabel = new JPanel();
+		JPanel panelText = new JPanel();
+		JPanel panelButton = new JPanel();
+		panelLabel.add(label);
+		panelText.add(textField);
+		panelButton.add(valider);
+		panelNbSujet.add(panelLabel);
+		panelNbSujet.add(panelText);
+		panelNbSujet.add(panelButton);
 		Ecouteur listen = new Ecouteur();
-		bouton1.addActionListener(listen);
-		frame.getContentPane().add(panel);
+		valider.addActionListener(listen);
+		valider2.addActionListener(listen);
+		valider3.addActionListener(listen);
+		methode1.addActionListener(listen);
+		methode2.addActionListener(listen);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.getContentPane().add(panelNbSujet);
 		frame.pack();
 		frame.setVisible(true);
 
@@ -47,21 +69,121 @@ public class ChoixTag {
 	public class Ecouteur implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			panel2.setLayout(new BoxLayout(panel2, BoxLayout.PAGE_AXIS));
-			methode1.add(bouton2);
-			methode2.add(bouton3);
-			panel2.add(methode1);
-			panel2.add(methode2);
-			frame.getContentPane().remove(panel);
-			frame.setTitle("Choix de la méthode");
-			frame.getContentPane().add(panel2);
-			frame.pack();
-			frame.setVisible(true);
+			if (e.getSource() == valider) {
+				try {
+					nbSujet = Integer.parseInt(textField.getText());
+					textField.setText("");
+					frame.getContentPane().remove(panelNbSujet);
+					JLabel label = new JLabel("Entrez le sujet que vous voulez chercher");
+					JPanel panelLabel = new JPanel();
+					JPanel panelText = new JPanel();
+					JPanel panelButton = new JPanel();
+					panelLabel.add(label);
+					panelText.add(textField);
+					panelButton.add(valider2);
+					panelNomSujet.add(panelLabel);
+					panelNomSujet.add(panelText);
+					panelNomSujet.add(panelButton);
+					frame.getContentPane().add(panelNomSujet);
+					frame.pack();
+					frame.setVisible(true);
+
+				} catch (NumberFormatException nfe) {
+					JPanel err = new JPanel();
+					err.add(erreur);
+					frame.getContentPane().remove(panelNbSujet);
+					panelNbSujet.add(err);
+					frame.getContentPane().add(panelNbSujet);
+					frame.pack();
+					frame.setVisible(true);
+				}
+
+			}
+			if (e.getSource() == valider2) {
+				if (textField.getText() != null)
+					sujet_list.add(textField.getText());
+				textField.setText("");
+				if (sujet_list.size() == nbSujet) {
+					frame.getContentPane().remove(panelNomSujet);
+					JLabel label = new JLabel("Entrez le nombre de resultat à afficher");
+					JPanel panelLabel = new JPanel();
+					JPanel panelText = new JPanel();
+					JPanel panelButton = new JPanel();
+					panelLabel.add(label);
+					panelText.add(textField);
+					panelButton.add(valider3);
+					panelNbResultat.add(panelLabel);
+					panelNbResultat.add(panelText);
+					panelNbResultat.add(panelButton);
+					frame.getContentPane().add(panelNbResultat);
+					frame.pack();
+					frame.setVisible(true);
+				}
+			}
+
+			if (e.getSource() == valider3) {
+				try {
+					Integer.parseInt(textField.getText());
+					nbResultat = textField.getText();
+					textField.setText("");
+					frame.getContentPane().remove(panelNbResultat);
+					JLabel label = new JLabel("Choisissez la méthode pour trier les résultats");
+					JPanel panelLabel = new JPanel();
+					JPanel panelButton1 = new JPanel();
+					JPanel panelButton2 = new JPanel();
+					panelLabel.add(label);
+					panelButton1.add(methode1);
+					panelButton2.add(methode2);
+					panelMethode.add(panelLabel);
+					panelMethode.add(panelButton1);
+					panelMethode.add(panelButton2);
+					frame.getContentPane().add(panelMethode);
+					frame.pack();
+					frame.setVisible(true);
+
+				} catch (NumberFormatException nfe) {
+					JPanel err = new JPanel();
+					err.add(erreur);
+					frame.getContentPane().remove(panelNbResultat);
+					panelNbResultat.add(err);
+					frame.getContentPane().add(panelNbResultat);
+					frame.pack();
+					frame.setVisible(true);
+				}
+
+			}
+			if (e.getSource() == methode1 || e.getSource() == methode2) {
+				if (e.getSource() == methode1)
+					choix = "1";
+				else
+					choix = "2";
+				frame.getContentPane().remove(panelMethode);
+				fonction_Dave f = new fonction_Dave();
+				String u_list = "";
+				try {
+					u_list = f.resultat(sujet_list, nbResultat, choix);
+				} catch (IOException e1) {
+
+					e1.printStackTrace();
+				} catch (JSONException e1) {
+					e1.printStackTrace();
+				}
+				JTextArea list_de_user = new JTextArea(u_list);
+				JPanel panel = new JPanel();
+				JScrollBar vbar = new JScrollBar(JScrollBar.VERTICAL, 30, 40, 0, 300);
+
+				panel.add(vbar, BorderLayout.EAST);
+				panel.add(list_de_user);
+				panelResulat.add(panel);
+				frame.getContentPane().add(panelResulat);
+				frame.pack();
+				frame.setVisible(true);
+			}
 		}
 
 	}
 
 	public static void main(String[] args) throws IOException, JSONException {
-		ChoixTag t = new ChoixTag();
+		new ChoixTag();
 	}
 }
--- src/fonctionalite_Dave/fonction_Dave.java
+++ src/fonctionalite_Dave/fonction_Dave.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.HashMap;
+
 import org.json.JSONException;
 
 import Base_de_Donnees.Operation_BDD;
@@ -12,12 +13,11 @@ import Connection_HTTP.Requete_Generateur;
 import Data_Structure.JSON_Converter;
 import Data_Structure.Tag;
 import Data_Structure.TagsAnswerers;
-import Data_Structure.User;
-import IO_operation.Input_Keyboard;
 
 public class fonction_Dave {
 
-	public static void main(String[] args) throws IOException, JSONException {
+	public static String resultat(ArrayList<String> sujet_list, String nombre, String choix)
+			throws IOException, JSONException {
 
 		String json_str_tags = HttpRequest.sendGet(Requete_Generateur.Fonction_Tags(),
 				Requete_Generateur.GET_Parameters(20));
@@ -29,7 +29,7 @@ public class fonction_Dave {
 		Connection Conn_BDD = Operation_BDD.Derby_Connexion();
 
 		Operation_BDD.Create_Table_Tags(Conn_BDD);
-		
+
 		Operation_BDD.Delete_Table_Tags(Conn_BDD);
 
 		for (Tag tag : Tags_List) {
@@ -41,60 +41,77 @@ public class fonction_Dave {
 		System.out.println(Tags_List_New);
 
 		// Operation_BDD.Derby_DisConnexion(Conn_BDD);
+		/*
+		 * String nb_sujet = Input_Keyboard.
+		 * Get_String("Veuillez saisir le nombre des sujets vous voulez chercher:"
+		 * );
+		 * 
+		 * ArrayList<String> sujet_list = new ArrayList<String>();
+		 * 
+		 * for (int i = 0; i < Integer.parseInt(nb_sujet); i++) {
+		 * sujet_list.add(Input_Keyboard.Get_String("Veuillez entrer un sujet:")
+		 * ); }
+		 */
+		// String sujet = Input_Keyboard.Get_String("Veuillez entrer un
+		// sujet.");
 
-		String nb_sujet = Input_Keyboard.Get_String("Veuillez saisir le nombre des sujets vous voulez chercher:");
-		
-		ArrayList<String> sujet_list = new ArrayList<String>();
-		
-		for (int i=0; i<Integer.parseInt(nb_sujet); i++) {
-			sujet_list.add(Input_Keyboard.Get_String("Veuillez entrer un sujet:"));
-		}
-			
-		//String sujet = Input_Keyboard.Get_String("Veuillez entrer un sujet.");
+		// String nombre = Input_Keyboard.Get_String("Veuillez entrer le nombre
+		// de personnes a chercher.");
 
-		String nombre = Input_Keyboard.Get_String("Veuillez entrer le nombre de personnes a chercher.");
-		
 		Operation_BDD.Create_Table_Users(Conn_BDD);
-		
+
 		Operation_BDD.Delete_Table_Users(Conn_BDD);
-		
+
 		Operation_BDD.Create_Tables_TagsAnswerers(Conn_BDD);
-        
-        Operation_BDD.Delete_Table_TagsAnswerers(Conn_BDD);
-		
-        for(String sujet : sujet_list) {
-        	
-        	String json_str_tags_answerers = HttpRequest.sendGet(Requete_Generateur.Fonction_Tags(1,sujet),Requete_Generateur.GET_Parameters(nombre));
-        	
-        	HashMap<String, Object> JSON_Map_tags_answerers = JSON_Converter.jsonToMap(json_str_tags_answerers);
-        	
-        	ArrayList<TagsAnswerers> TagsAnswerers_List = TagsAnswerers.JSON_ListtoTagsAnswerers_List((ArrayList)JSON_Map_tags_answerers.get("items"), sujet);
-        	
-        	for(TagsAnswerers tag_answerers : TagsAnswerers_List) {
-        		Operation_BDD.Insert_Into_Table_Users(Conn_BDD, tag_answerers.getUser());
-        		Operation_BDD.Insert_Into_Table_TagsAnswerers(Conn_BDD, tag_answerers);
-        	}
-        }
 
-		String choix = Input_Keyboard.Get_String(
-				"Veuillez choisir la methode pour trier:\n1. Par le nombre de poser des reponses\n2. Par des scores qu'il est recu");
-		
-		//Operation_BDD.Derby_DisConnexion(Conn_BDD);
+		Operation_BDD.Delete_Table_TagsAnswerers(Conn_BDD);
 
-		ArrayList<TagsAnswerers> TagsAnswerers_List_new = Operation_BDD.Select_Table_PluralTagsAnswerers(Conn_BDD, choix);
-		
-		//System.out.println(TagsAnswerers_List_new);
-		
-		for(TagsAnswerers tag_answerers : TagsAnswerers_List_new) {
+		for (String sujet : sujet_list) {
+
+			String json_str_tags_answerers = HttpRequest.sendGet(Requete_Generateur.Fonction_Tags(1, sujet),
+					Requete_Generateur.GET_Parameters(nombre));
+
+			HashMap<String, Object> JSON_Map_tags_answerers = JSON_Converter.jsonToMap(json_str_tags_answerers);
+
+			ArrayList<TagsAnswerers> TagsAnswerers_List = TagsAnswerers
+					.JSON_ListtoTagsAnswerers_List((ArrayList) JSON_Map_tags_answerers.get("items"), sujet);
+
+			for (TagsAnswerers tag_answerers : TagsAnswerers_List) {
+				Operation_BDD.Insert_Into_Table_Users(Conn_BDD, tag_answerers.getUser());
+				Operation_BDD.Insert_Into_Table_TagsAnswerers(Conn_BDD, tag_answerers);
+			}
+		}
+		/*
+		 * String choix = Input_Keyboard.Get_String(
+		 * "Veuillez choisir la methode pour trier:\n1. Par le nombre de poser des reponses\n2. Par des scores qu'il est recu"
+		 * );
+		 */
+		// Operation_BDD.Derby_DisConnexion(Conn_BDD);
+
+		ArrayList<TagsAnswerers> TagsAnswerers_List_new = Operation_BDD.Select_Table_PluralTagsAnswerers(Conn_BDD,
+				choix);
+
+		// System.out.println(TagsAnswerers_List_new);
+		String s = "";
+		for (TagsAnswerers tag_answerers : TagsAnswerers_List_new) {
+
+			s = s + "User_pseudo:" + tag_answerers.getUser().getDisplay_name() + "\n";
+			s = s + "User_Link:" + tag_answerers.getUser().getLink() + "\n";
+			s = s + "Tags_name:" + sujet_list + "\n";
+			s = s + "User_post_count (total):" + tag_answerers.getPost_count() + "\n";
+			s = s + "User_score (total):" + tag_answerers.getScore() + "\n\n";
+
 			System.out.println("*****************************************");
 			System.out.println("User_pseudo:" + tag_answerers.getUser().getDisplay_name());
 			System.out.println("User_Link:" + tag_answerers.getUser().getLink());
 			System.out.println("Tags_name:" + sujet_list);
 			System.out.println("User_post_count (total):" + tag_answerers.getPost_count());
 			System.out.println("User_score (total):" + tag_answerers.getScore());
+
 		}
-		
+
 		Operation_BDD.Derby_DisConnexion(Conn_BDD);
+		return s;
 		// resultat_Frame f = new resultat_Frame("Fonction_Dave",result);
 	}
 }
