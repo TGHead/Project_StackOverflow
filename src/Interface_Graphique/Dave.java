b10c61eee1e221037b2778c02403f51fe945b324
ALEXIS <ALEXIS@192.168.1.12>
2016-11-28 18:59:00 +0100

    Ajout interface Alice1 + BugFix Alice1

--- src/Interface_Graphique/Dave.java
+++ src/Interface_Graphique/Dave.java
@@ -25,20 +25,27 @@ import javax.swing.event.HyperlinkListener;
 
 import org.json.JSONException;
 
+import fonctionalite_Alice.fonction_Alice;
 import fonctionalite_Dave.fonction_Dave;
 
 public class Dave {
 
 	fonction_Dave f = new fonction_Dave();
+	fonction_Alice f2 = new fonction_Alice();
 	JFrame frame = new JFrame("Dave");
 	JLabel label = new JLabel("Entrez le(s) sujet(s) à rechercher");
 	JPanel panel = new JPanel();
 	JScrollPane jsp = new JScrollPane();
 	JButton button = new JButton("OK");
 	JTextField text = new JTextField(20);
-	String s[] = { "Trier par score obtenu", "Trier par nombre de post" };
-	JComboBox<String> box = new JComboBox<String>(s);
+	String listeDave[] = { "Trier par score obtenu", "Trier par nombre de post" };
+	String listeAlice[] = { "Chercher des nouvelles questions", "Chercher les utilisateurs ayant plus de badges",
+			"Trier les questions auxquelles j'ai répondu" };
+	String listeFonctions[] = { "Fonctionnalités de Dave", "Fonctionnalités d'Alice" };
+	JComboBox<String> box = new JComboBox<String>(listeDave);
+	JComboBox<String> boxFonctions = new JComboBox<String>(listeFonctions);
 	JEditorPane result = new JEditorPane();
+	boolean res = false;
 	ArrayList<String> list = new ArrayList<String>();
 	ArrayList<String[]> tab = new ArrayList<String[]>();
 	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -49,17 +56,22 @@ public class Dave {
 		JPanel panel1 = new JPanel();
 		JPanel panel2 = new JPanel();
 		JPanel panel3 = new JPanel();
+		JPanel panel4 = new JPanel();
+		panel4.add(boxFonctions);
 		panel1.add(text);
 		panel2.add(box);
 		panel2.add(button);
 		panel3.add(label);
+		panel.add(panel4);
 		panel.add(panel3);
 		panel.add(panel1);
 		panel.add(panel2);
+
 		Ecouteur listen = new Ecouteur();
+		EcouteurFonctions lis = new EcouteurFonctions();
 		EcouteurURL listener = new EcouteurURL();
 		result.addHyperlinkListener(listener);
-
+		boxFonctions.addActionListener(lis);
 		button.addActionListener(listen);
 		frame.getContentPane().add(panel);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -89,53 +101,134 @@ public class Dave {
 	public class Ecouteur implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			result.setText("");
-			String recherche = text.getText();
-			String[] temp = recherche.split("[,\\ \\;]");
-			list.clear();
-			for (int i = 0; i < temp.length; i++) {
-				if (!temp[i].isEmpty())
-					try {
-						list.add(URLEncoder.encode(temp[i], "UTF-8"));
-					} catch (UnsupportedEncodingException e1) {
-						e1.printStackTrace();
+			int n = boxFonctions.getSelectedIndex();
+			switch (n) {
+			case 0:
+				result.setText("");
+				String recherche = text.getText();
+				String[] temp = recherche.split("[,\\ \\;]");
+				list.clear();
+				for (int i = 0; i < temp.length; i++) {
+					if (!temp[i].isEmpty())
+						try {
+							list.add(URLEncoder.encode(temp[i], "UTF-8"));
+						} catch (UnsupportedEncodingException e1) {
+							e1.printStackTrace();
+						}
+				}
+
+				String choix = Integer.toString(box.getSelectedIndex());
+				tab.clear();
+				try {
+					tab = f.resultat(list, "20", choix);
+					res = true;
+				} catch (IOException e1) {
+
+					e1.printStackTrace();
+				} catch (JSONException e1) {
+					e1.printStackTrace();
+				}
+
+				String s = "";
+				if (tab.get(tab.size() - 1)[0] != "") {
+					s = "<b style=\"color:#FF0000\">Le(s) tag(s) " + tab.get(tab.size() - 1)[0]
+							+ " n'existe(nt) pas</b><br>";
+				}
+
+				for (int i = 0; i < tab.size() - 1; i++) {
+					s = s + "<a href=\"" + tab.get(i)[1] + "\">" + tab.get(i)[0] + "</a><br>Nombre de post : "
+							+ tab.get(i)[2] + "<br>Score obtenu : " + tab.get(i)[3] + "<br><br>";
+				}
+
+				result.setText(s);
+				frame.getContentPane().remove(panel);
+				jsp.setViewportView(result);
+				panel.add(jsp);
+				result.setCaretPosition(0);
+				result.setEditable(false);
+				frame.getContentPane().add(panel);
+				frame.setSize(251, (int) (screenSize.getHeight() - 100));
+				if (tab.get(tab.size() - 1) == tab.get(0)) {
+					res = false;
+					frame.pack();
+				}
+				frame.setVisible(true);
+				break;
+			case 1:
+				ArrayList<String[]> tab = new ArrayList<String[]>();
+				try {
+					tab = f2.Alice1(text.getText());
+					res = true;
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (JSONException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+				String string = "";
+
+				if (tab.isEmpty()) {
+					res = false;
+					string = "<b style=\"color:#FF0000\">Le numéro d'identifiant " + text.getText()
+							+ " n'existe pas</b><br>";
+				} else {
+
+					for (int i = 0; i < tab.get(0).length; i++) {
+						string = string + "<a href=\"" + tab.get(1)[i] + "\">" + tab.get(0)[i] + "</a><br>Tags :"
+								+ tab.get(2)[i] + "<br><br>";
+
 					}
+				}
+				result.setText(string);
+				frame.getContentPane().remove(panel);
+				jsp.setViewportView(result);
+				panel.add(jsp);
+				result.setCaretPosition(0);
+				result.setEditable(false);
+				frame.getContentPane().add(panel);
+				if (res)
+					frame.setSize(373, (int) (screenSize.getHeight() - 100));
+				else
+					frame.pack();
+
+				frame.setVisible(true);
+				break;
+
+			default:
+				break;
+			}
+		}
+	}
+
+	public class EcouteurFonctions implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			int n = boxFonctions.getSelectedIndex();
+			switch (n) {
+			case 0:
+				frame.setTitle("Dave");
+				label.setText("Entrez le(s) sujet(s) à rechercher");
+				box.setModel(new JComboBox(listeDave).getModel());
+				if (res)
+					frame.setSize(251, (int) (screenSize.getHeight() - 100));
+				else
+					frame.pack();
+				break;
+			case 1:
+				frame.setTitle("Alice");
+				label.setText("Entrez votre numéro d'identifiant");
+				box.setModel(new JComboBox(listeAlice).getModel());
+				if (res)
+					frame.setSize(373, (int) (screenSize.getHeight() - 100));
+				else
+					frame.pack();
+				break;
+			default:
+				break;
 			}
 
-			String choix = Integer.toString(box.getSelectedIndex());
-			tab.clear();
-			try {
-				tab = f.resultat(list, "20", choix);
-			} catch (IOException e1) {
-
-				e1.printStackTrace();
-			} catch (JSONException e1) {
-				e1.printStackTrace();
-			}
-
-			String s = "";
-			if (tab.get(tab.size() - 1)[0] != "") {
-				s = "<b style=\"color:#FF0000\">Le(s) tag(s) " + tab.get(tab.size() - 1)[0]
-						+ " n'existe(nt) pas</b><br>";
-			}
-
-			for (int i = 0; i < tab.size() - 1; i++) {
-				s = s + "<a href=\"" + tab.get(i)[1] + "\">" + tab.get(i)[0] + "</a><br>Nombre de post : "
-						+ tab.get(i)[2] + "<br>Score obtenu : " + tab.get(i)[3] + "<br><br>";
-			}
-
-			result.setText(s);
-			frame.getContentPane().remove(panel);
-			jsp.setViewportView(result);
-			panel.add(jsp);
-			result.setCaretPosition(0);
-			result.setEditable(false);
-			frame.getContentPane().add(panel);
-			frame.setSize(251, (int) (screenSize.getHeight() - 100));
-			if (tab.get(tab.size() - 1) == tab.get(0))
-				frame.pack();
-
-			frame.setVisible(true);
 		}
 	}
 
--- src/fonctionalite_Alice/fonction_Alice.java
+++ src/fonctionalite_Alice/fonction_Alice.java
@@ -1,110 +1,120 @@
 package fonctionalite_Alice;
 
 import java.io.IOException;
-import java.lang.System;
-import java.lang.reflect.Array;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Scanner;
 
 import org.json.JSONException;
 
 import Connection_HTTP.HttpRequest;
 import Data_Structure.JSON_Converter;
-import Data_Structure.Tag;
 
 public class fonction_Alice {
-	public static void main(String[] args) throws IOException, JSONException {
-		Scanner sc = new Scanner(System.in);
-		String id;
-		System.out.println("Veuillez entrer votre id.");
-		id = sc.next();
+	public static ArrayList<String[]> Alice1(String id) throws IOException, JSONException {
+		ArrayList<String[]> tab = new ArrayList<String[]>();
 		String req;
 		req = id + "/top-tags?page=1&pagesize=3&site=stackoverflow";
 		String json_str_top_tags = HttpRequest.sendGet("https://api.stackexchange.com/2.2/users/", req, false);
 		HashMap<String, Object> JSON_Map_tags = JSON_Converter.jsonToMap(json_str_top_tags);
 		Object it = JSON_Map_tags.get("items");
 		String temp = it.toString();
+
+		if (temp == "[]")
+			return tab;
+
 		int debut = temp.indexOf("tag_name");
 		int fin = temp.indexOf(" ", debut);
-		String tag1 = temp.substring(debut+9, fin);
+		String tag1 = temp.substring(debut + 9, fin);
 		debut = temp.indexOf("tag_name", fin);
 		fin = temp.indexOf(" ", debut);
-		String tag2 = temp.substring(debut+9, fin);
+		String tag2 = temp.substring(debut + 9, fin);
 		debut = temp.indexOf("tag_name", fin);
 		fin = temp.indexOf(" ", debut);
-		String tag3 = temp.substring(debut+9, fin);
+		String tag3 = temp.substring(debut + 9, fin);
+		tag1 = URLEncoder.encode(tag1, "UTF-8");
+		tag2 = URLEncoder.encode(tag2, "UTF-8");
+		tag3 = URLEncoder.encode(tag3, "UTF-8");
 		req = "page=1&pagesize=7&order=desc&sort=activity&tagged=" + tag1 + "&site=stackoverflow";
 		String questions = HttpRequest.sendGet("https://api.stackexchange.com/2.2/questions/unanswered", req, true);
-		System.out.println(questions);
-		String titres[] = new String[20];//Je stoque les titre là
-		String liens[] = new String[20];//Je stoque les liens là
-		String tags[] = new String[20];//Je stoque les tags là
-		for(int i = 0; i<7; i++){
-			titres[i] = Qtitle(questions, i+1);
-			System.out.println("question: ");
-			System.out.println(titres[i]);
-			liens[i] = Qlink(questions, i+1);
-			System.out.println(liens[i]);
-			tags[i] = Qtags(questions, i+1);
-			System.out.println("tags: ");
-			System.out.println(tags[i]);
+		// System.out.println(questions);
+		String titres[] = new String[20];// Je stoque les titre là
+		String liens[] = new String[20];// Je stoque les liens là
+		String tags[] = new String[20];// Je stoque les tags là
+		for (int i = 0; i < 7; i++) {
+			titres[i] = Qtitle(questions, i + 1);
+			// System.out.println("question: ");
+			// System.out.println(titres[i]);
+			liens[i] = Qlink(questions, i + 1);
+			// System.out.println(liens[i]);
+			tags[i] = Qtags(questions, i + 1);
+			// System.out.println("tags: ");
+			// System.out.println(tags[i]);
 		}
 		req = "page=1&pagesize=7&order=desc&sort=activity&tagged=" + tag2 + "&site=stackoverflow";
 		questions = HttpRequest.sendGet("https://api.stackexchange.com/2.2/questions/unanswered", req, true);
-		System.out.println(questions);
-		for(int i = 0; i<7; i++){
-			titres[i+7] = Qtitle(questions, i+1);
-			System.out.println("question: ");
-			System.out.println(titres[i+7]);
-			liens[i+7] = Qlink(questions, i+1);
-			System.out.println(liens[i+7]);
-			tags[i+7] = Qtags(questions, i+1);
-			System.out.println("tags: ");
-			System.out.println(tags[i+7]);
+		// System.out.println(questions);
+		for (int i = 0; i < 7; i++) {
+			titres[i + 7] = Qtitle(questions, i + 1);
+			// System.out.println("question: ");
+			// System.out.println(titres[i + 7]);
+			liens[i + 7] = Qlink(questions, i + 1);
+			// System.out.println(liens[i + 7]);
+			tags[i + 7] = Qtags(questions, i + 1);
+			// System.out.println("tags: ");
+			// System.out.println(tags[i + 7]);
 		}
 		req = "page=1&pagesize=7&order=desc&sort=activity&tagged=" + tag3 + "&site=stackoverflow";
 		questions = HttpRequest.sendGet("https://api.stackexchange.com/2.2/questions/unanswered", req, true);
-		System.out.println(questions);
-		for(int i = 0; i<6; i++){
-			titres[i+14] = Qtitle(questions, i+1);
-			System.out.println("question: ");
-			System.out.println(titres[i+14]);
-			liens[i+14] = Qlink(questions, i+1);
-			System.out.println(liens[i+14]);
-			tags[i+14] = Qtags(questions, i+1);
-			System.out.println("tags: ");
-			System.out.println(tags[i+14]);
+		// System.out.println(questions);
+		for (int i = 0; i < 6; i++) {
+			titres[i + 14] = Qtitle(questions, i + 1);
+			// System.out.println("question: ");
+			// System.out.println(titres[i + 14]);
+			liens[i + 14] = Qlink(questions, i + 1);
+			// System.out.println(liens[i + 14]);
+			tags[i + 14] = Qtags(questions, i + 1);
+			// System.out.println("tags: ");
+			// System.out.println(tags[i + 14]);
 		}
+		tab.clear();
+		tab.add(titres);
+		tab.add(liens);
+		tab.add(tags);
+		return (tab);
+
 	}
-	public static String Qtags(String q, int num){
+
+	public static String Qtags(String q, int num) {
 		int debut = 0;
 		int fin = 0;
-		for(int i=0; i<num; i++){
+		for (int i = 0; i < num; i++) {
 			debut = q.indexOf("tags", fin);
 			fin = q.indexOf("]", debut);
 		}
-		String res = q.substring(debut+7, fin);
+		String res = q.substring(debut + 7, fin);
 		return res;
 	}
-	public static String Qlink(String q, int num){
+
+	public static String Qlink(String q, int num) {
 		int debut = 0;
 		int fin = 0;
-		for(int i=0; i<num; i++){
-			debut = q.indexOf("link", fin);
+		for (int i = 0; i < num; i++) {
+			debut = q.indexOf("link\":\"http://stackoverflow.com/questions", fin);
 			fin = q.indexOf("}", debut);
 		}
-		String res = q.substring(debut+6, fin);
+		String res = q.substring(debut + 7, fin);
 		return res;
 	}
-	public static String Qtitle(String q, int num){
+
+	public static String Qtitle(String q, int num) {
 		int debut = 0;
 		int fin = 0;
-		for(int i=0; i<num; i++){
+		for (int i = 0; i < num; i++) {
 			debut = q.indexOf("title", fin);
 			fin = q.indexOf("}", debut);
 		}
-		String res = q.substring(debut+8, fin-1);
+		String res = q.substring(debut + 8, fin - 1);
 		return res;
 	}
 }
